f8f5eff00c4e0f9fcba7701b26ea28c5741dd5e4
Louis Beaumont <louis.beaumont@gmail.com>
2022-10-30 13:21:03 +0100

    fix linting, exports

--- main.tsx
+++ main.tsx
@@ -1,18 +1,33 @@
 import {ThemeProvider} from "@mui/material";
-import {App, Notice, Plugin, PluginSettingTab, Editor} from "obsidian";
+import {App, Editor, Notice, Plugin, PluginSettingTab} from "obsidian";
+import {OpenAIApi} from "openai";
 import * as React from "react";
 import {createRoot, Root} from "react-dom/client";
-import {generateAsync} from "stableDiffusion";
+import {DraftStabilityOptions, generateAsync,
+  RequiredStabilityOptions, ResponseData} from "stableDiffusion";
 import {AvaSettings, CustomSettings, DEFAULT_SETTINGS} from "./Settings";
 import {AvaSuggest, StatusBar} from "./suggest";
 import {theme} from "./theme";
+
+interface StableDiffusion {
+  generateAsync: (
+    opts: DraftStabilityOptions & RequiredStabilityOptions
+) => {
+    images: ImageData[];
+    res: ResponseData;
+};
+}
 
 
 // eslint-disable-next-line require-jsdoc
 export default class AvaPlugin extends Plugin {
   settings: AvaSettings;
   statusBarItem: Root;
+  openai: OpenAIApi;
+  stableDiffusion: StableDiffusion;
 
+
+  // eslint-disable-next-line require-jsdoc
   async onload() {
     await this.loadSettings();
     const statusBarItemHtml = this.addStatusBarItem();
@@ -21,7 +36,8 @@ export default class AvaPlugin extends Plugin {
     const suggest = new AvaSuggest(this.app, this, 1000, 3);
     this.openai = suggest.openai;
     this.stableDiffusion = {
-      generateAsync : generateAsync,
+      // @ts-ignore
+      generateAsync: generateAsync,
     };
     // This adds a simple command that can be triggered anywhere
     this.addCommand({
@@ -32,77 +48,86 @@ export default class AvaPlugin extends Plugin {
         suggest.setAutomaticSuggestion(!this.settings.openai.automatic);
       },
     });
-	  this.addCommand({
+    this.addCommand({
       id: "ava-generate-image",
       name: "Generate an image based on selected text",
       editorCallback: async (editor: Editor) => {
-		if (!this.settings.stableDiffusion.key) {
-			new Notice("You need to set a key for Stable Diffusion in the settings", 3000);
-			return;
-		}
-		if (!window.getSelection().toString()) return;
-        
-		const outDir = (this.app.vault.adapter as any).basePath + "/" + this.app.workspace.getActiveFile().parent.path;
-		this.statusBarItem.render(
-			<StatusBar
-				status="loading"
-			/>
-		);
-		// this.app.vault.createFolder(outDir);
-		// @ts-ignore
-		try {
-			const {res, images} = await generateAsync({
-				prompt: window.getSelection().toString(),
-				apiKey: this.settings.stableDiffusion.key,
-				outDir: outDir,
-				debug: false,
-				samples: 1,
-			});
-			// append image below
-			editor.replaceSelection(
-				`${window.getSelection().toString()}\n\n![[${images[0].filePath.split("/").pop()}]]\n\n`
-			);
-			
-			this.statusBarItem.render(
-				<StatusBar
-				status="success"
-				statusMessage="Completion successful"
-				/>
-			);
-		} catch (e) {
-			this.statusBarItem.render(
-				<StatusBar
-				  status="error"
-				  statusMessage={"Error while generating image " + e}
-				/>
-			);
-		}
-        
+        if (!this.settings.stableDiffusion.key) {
+          new Notice(
+              "You need to set a key for Stable Diffusion in the settings",
+              3000);
+          return;
+        }
+        if (!window.getSelection().toString()) return;
+
+        const outDir = (this.app.vault.adapter as any).basePath + "/" +
+        this.app.workspace.getActiveFile().parent.path;
+        this.statusBarItem.render(
+            <StatusBar
+              status="loading"
+            />
+        );
+        try {
+          const {images} = await generateAsync({
+            prompt: window.getSelection().toString(),
+            apiKey: this.settings.stableDiffusion.key,
+            outDir: outDir,
+            debug: false,
+            samples: 1,
+          });
+          // append image below
+          editor.replaceSelection(
+              // eslint-disable-next-line max-len
+              `${window.getSelection().toString()}\n\n![[${images[0].filePath.split("/").pop()}]]\n\n`
+          );
+
+          this.statusBarItem.render(
+              <StatusBar
+                status="success"
+                statusMessage="Completion successful"
+              />
+          );
+        } catch (e) {
+          this.statusBarItem.render(
+              <StatusBar
+                status="error"
+                statusMessage={"Error while generating image " + e}
+              />
+          );
+        }
       },
     });
     this.registerEditorSuggest(suggest);
 
-    // This adds a settings tab so the user can configure various aspects of the plugin
+    // This adds a settings tab so the user
+    // can configure various aspects of the plugin
     this.addSettingTab(new AvaSettingTab(this.app, this));
   }
 
+  // eslint-disable-next-line require-jsdoc
   async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    this.settings = Object.assign({},
+        DEFAULT_SETTINGS, await this.loadData());
   }
 
+  // eslint-disable-next-line require-jsdoc
   async saveSettings() {
-    await this.saveData(this.settings);
+    await this.saveData(
+        this.settings);
   }
 }
 
 
+// eslint-disable-next-line require-jsdoc
 class AvaSettingTab extends PluginSettingTab {
   plugin: AvaPlugin;
+  // eslint-disable-next-line require-jsdoc
   constructor(app: App, plugin: AvaPlugin) {
     super(app, plugin);
     this.plugin = plugin;
   }
 
+  // eslint-disable-next-line require-jsdoc
   display(): void {
     const root = createRoot(this.containerEl);
     root.render(
--- stableDiffusion.ts
+++ stableDiffusion.ts
@@ -1,238 +1,242 @@
-import { grpc } from '@improbable-eng/grpc-web'
-import { NodeHttpTransport } from '@improbable-eng/grpc-web-node-http-transport'
-import { EventEmitter } from 'events'
-import fs from 'fs'
-import mime from 'mime'
-import path from 'path'
+import {grpc} from "@improbable-eng/grpc-web";
+import {NodeHttpTransport} from "@improbable-eng/grpc-web-node-http-transport";
+import {EventEmitter} from "events";
+import fs from "fs";
+import mime from "mime";
+import path from "path";
 import {
-    Answer,
-    ArtifactType, ClassifierParameters, ImageParameters, Prompt, Request, SamplerParameters, StepParameter, TransformType
-} from 'stability-sdk/gooseai/generation/generation_pb'
-import { GenerationService } from 'stability-sdk/gooseai/generation/generation_pb_service'
-import TypedEmitter from 'typed-emitter'
-import uuid4 from 'uuid4'
-
-import { DiffusionSampler } from 'stability-sdk/gooseai/generation/generation_pb'
-
+  Answer,
+  ArtifactType, ClassifierParameters, DiffusionSampler,
+  ImageParameters, Prompt, Request, SamplerParameters, StepParameter,
+  TransformType,
+} from "stability-sdk/gooseai/generation/generation_pb";
+import {GenerationService} from
+  "stability-sdk/gooseai/generation/generation_pb_service";
+import TypedEmitter from "typed-emitter";
+import uuid4 from "uuid4";
 
 
 export const range = (min: number, max: number) =>
-    Math.floor(Math.random() * (max - min + 1)) + min
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const diffusionMap = {
-    ddim: DiffusionSampler.SAMPLER_DDIM,
-    plms: DiffusionSampler.SAMPLER_DDPM,
-    k_euler: DiffusionSampler.SAMPLER_K_EULER,
-    k_euler_ancestral: DiffusionSampler.SAMPLER_K_EULER_ANCESTRAL,
-    k_heun: DiffusionSampler.SAMPLER_K_HEUN,
-    k_dpm_2: DiffusionSampler.SAMPLER_K_DPM_2,
-    k_dpm_2_ancestral: DiffusionSampler.SAMPLER_K_DPM_2_ANCESTRAL,
-    k_lms: DiffusionSampler.SAMPLER_K_LMS,
-}
+  ddim: DiffusionSampler.SAMPLER_DDIM,
+  plms: DiffusionSampler.SAMPLER_DDPM,
+  k_euler: DiffusionSampler.SAMPLER_K_EULER,
+  k_euler_ancestral: DiffusionSampler.SAMPLER_K_EULER_ANCESTRAL,
+  k_heun: DiffusionSampler.SAMPLER_K_HEUN,
+  k_dpm_2: DiffusionSampler.SAMPLER_K_DPM_2,
+  k_dpm_2_ancestral: DiffusionSampler.SAMPLER_K_DPM_2_ANCESTRAL,
+  k_lms: DiffusionSampler.SAMPLER_K_LMS,
+};
 
-type DraftStabilityOptions = Partial<{
-    outDir: string
-    debug: boolean
-    requestId: string
-    samples: number
-    engine: 'stable-diffusion-v1'
-    host: string
-    seed: number
-    width: number
-    height: number
-    diffusion: keyof typeof diffusionMap
-    steps: number
-    cfgScale: number
-    noStore: boolean
+export type DraftStabilityOptions = Partial<{
+    outDir: string;
+    debug: boolean;
+    requestId: string;
+    samples: number;
+    engine: "stable-diffusion-v1";
+    host: string;
+    seed: number;
+    width: number;
+    height: number;
+    diffusion: keyof typeof diffusionMap;
+    steps: number;
+    cfgScale: number;
+    noStore: boolean;
 }>
 
-type RequiredStabilityOptions = {
-    apiKey: string
-    prompt: string
+export type RequiredStabilityOptions = {
+    apiKey: string;
+    prompt: string;
 }
 
 type StabilityOptions = RequiredStabilityOptions &
     Required<DraftStabilityOptions>
 
 type ImageData = { buffer: Buffer; filePath: string }
-type ResponseData = {
-    isOk: boolean
-    status: keyof grpc.Code
-    code: grpc.Code
-    message: string
-    trailers: grpc.Metadata
+export type ResponseData = {
+    isOk: boolean;
+    status: keyof grpc.Code;
+    code: grpc.Code;
+    message: string;
+    trailers: grpc.Metadata;
 }
 
 type StabilityApi = TypedEmitter<{
-    image: (data: ImageData) => void
-    end: (data: ResponseData) => void
+    image: (data: ImageData) => void;
+    end: (data: ResponseData) => void;
 }>
 
 const withDefaults: (
     draftOptions: DraftStabilityOptions & RequiredStabilityOptions
 ) => StabilityOptions = (draft) => {
-    if (!draft.prompt) throw new Error('Prompt is required')
+  if (!draft.prompt) throw new Error("Prompt is required");
 
-    const requestId = draft.requestId ?? uuid4()
-    return {
-        ...draft,
-        host: draft.host ?? 'https://grpc.stability.ai:443',
-        engine: draft.engine ?? 'stable-diffusion-v1',
-        requestId,
-        seed: draft.seed ?? range(0, 4294967295),
-        width: draft.width ?? 512,
-        height: draft.height ?? 512,
-        diffusion: draft.diffusion ?? 'k_lms',
-        steps: draft.steps ?? 50,
-        cfgScale: draft.cfgScale ?? 7,
-        samples: draft.samples ?? 1,
-        outDir: draft.outDir ?? path.join(process.cwd(), '.out', requestId),
-        debug: Boolean(draft.debug),
-        noStore: Boolean(draft.noStore),
-    }
-}
+  const requestId = draft.requestId ?? uuid4();
+  return {
+    ...draft,
+    host: draft.host ?? "https://grpc.stability.ai:443",
+    engine: draft.engine ?? "stable-diffusion-v1",
+    requestId,
+    seed: draft.seed ?? range(0, 4294967295),
+    width: draft.width ?? 512,
+    height: draft.height ?? 512,
+    diffusion: draft.diffusion ?? "k_lms",
+    steps: draft.steps ?? 50,
+    cfgScale: draft.cfgScale ?? 7,
+    samples: draft.samples ?? 1,
+    outDir: draft.outDir ?? path.join(process.cwd(), ".out", requestId),
+    debug: Boolean(draft.debug),
+    noStore: Boolean(draft.noStore),
+  };
+};
 
 export const generate: (
     opts: DraftStabilityOptions & RequiredStabilityOptions
 ) => StabilityApi = (opts) => {
-    const {
-        host,
-        engine,
-        requestId,
-        seed,
-        width,
-        height,
-        diffusion,
-        steps,
-        cfgScale,
-        samples,
-        outDir,
-        prompt: promptText,
-        apiKey,
-        noStore,
-        debug,
-    } = withDefaults(opts)
+  const {
+    host,
+    engine,
+    requestId,
+    seed,
+    width,
+    height,
+    diffusion,
+    steps,
+    cfgScale,
+    samples,
+    outDir,
+    prompt: promptText,
+    apiKey,
+    noStore,
+    debug,
+  } = withDefaults(opts);
 
-    if (!promptText) throw new Error('Prompt text is required')
+  if (!promptText) throw new Error("Prompt text is required");
 
-    const api = new EventEmitter() as StabilityApi
+  const api = new EventEmitter() as StabilityApi;
 
-    // if (!noStore) mkdirp.sync(outDir)
+  // if (!noStore) mkdirp.sync(outDir)
 
-    /** Build Request **/
-    const request = new Request()
-    request.setEngineId(engine)
-    request.setRequestId(requestId)
+  /** Build Request **/
+  const request = new Request();
+  request.setEngineId(engine);
+  request.setRequestId(requestId);
 
-    const prompt = new Prompt()
-    prompt.setText(promptText)
-    request.addPrompt(prompt)
+  const prompt = new Prompt();
+  prompt.setText(promptText);
+  request.addPrompt(prompt);
 
-    const image = new ImageParameters()
-    image.setWidth(width)
-    image.setHeight(height)
-    image.setSeedList([seed])
+  const image = new ImageParameters();
+  image.setWidth(width);
+  image.setHeight(height);
+  image.setSeedList([seed]);
 
-    image.setSteps(steps)
-    image.setSamples(samples)
+  image.setSteps(steps);
+  image.setSamples(samples);
 
-    const transform = new TransformType()
-    transform.setDiffusion(diffusionMap[diffusion])
-    image.setTransform(transform)
+  const transform = new TransformType();
+  transform.setDiffusion(diffusionMap[diffusion]);
+  image.setTransform(transform);
 
-    const step = new StepParameter()
-    step.setScaledStep(0)
+  const step = new StepParameter();
+  step.setScaledStep(0);
 
-    const sampler = new SamplerParameters()
-    sampler.setCfgScale(cfgScale)
-    step.setSampler(sampler)
+  const sampler = new SamplerParameters();
+  sampler.setCfgScale(cfgScale);
+  step.setSampler(sampler);
 
-    image.addParameters(step)
+  image.addParameters(step);
 
-    request.setImage(image)
+  request.setImage(image);
 
-    const classifier = new ClassifierParameters()
-    // TODO: hack
-    // request.setClassifier(classifier)
-    /** End Build Request **/
+  // eslint-disable-next-line no-unused-vars
+  const classifier = new ClassifierParameters();
+  // TODO: hack
+  // request.setClassifier(classifier)
+  /** End Build Request **/
 
-    if (debug) {
-        console.log(
-            '[stability - request]',
-            JSON.stringify(request.toObject(), null, 2)
-        )
-    }
+  if (debug) {
+    console.log(
+        "[stability - request]",
+        JSON.stringify(request.toObject(), null, 2)
+    );
+  }
 
-    grpc.invoke(GenerationService.Generate, {
-        request,
-        host,
-        metadata: new grpc.Metadata({ Authorization: `Bearer ${apiKey}` }),
-        transport: NodeHttpTransport(),
-        onEnd: (code, message, trailers) => {
-            api.emit('end', {
-                isOk: code === grpc.Code.OK,
-                status: grpc.Code[code] as keyof grpc.Code,
-                code,
-                message,
-                trailers,
-            })
-        },
-        onMessage: (message: Answer) => {
-            const answer = message.toObject()
+  grpc.invoke(GenerationService.Generate, {
+    request,
+    host,
+    metadata: new grpc.Metadata({Authorization: `Bearer ${apiKey}`}),
+    // eslint-disable-next-line new-cap
+    transport: NodeHttpTransport(),
+    onEnd: (code, message, trailers) => {
+      api.emit("end", {
+        isOk: code === grpc.Code.OK,
+        status: grpc.Code[code] as keyof grpc.Code,
+        code,
+        message,
+        trailers,
+      });
+    },
+    onMessage: (message: Answer) => {
+      const answer = message.toObject();
 
-            if (answer.artifactsList) {
-                answer.artifactsList.forEach(
-                    ({ id, type, mime: mimeType, binary, seed: innerSeed }) => {
-                        if (type === ArtifactType.ARTIFACT_IMAGE) {
-                            // @ts-ignore
-                            const buffer = Buffer.from(binary, 'base64')
-                            const filePath = path.resolve(
-                                path.join(
-                                    outDir,
-                                    `${answer.answerId}-${id}-${innerSeed}.${mime.getExtension(
-                                        mimeType
-                                    )}`
-                                )
-                            )
+      if (answer.artifactsList) {
+        answer.artifactsList.forEach(
+            ({id, type, mime: mimeType, binary, seed: innerSeed}) => {
+              if (type === ArtifactType.ARTIFACT_IMAGE) {
+                // @ts-ignore
+                const buffer = Buffer.from(binary, "base64");
+                const filePath = path.resolve(
+                    path.join(
+                        outDir,
+                        // eslint-disable-next-line max-len
+                        `${answer.answerId}-${id}-${innerSeed}.${mime.getExtension(
+                            mimeType
+                        )}`
+                    )
+                );
 
-                            if (!noStore) fs.writeFileSync(filePath, buffer)
+                if (!noStore) fs.writeFileSync(filePath, buffer);
 
-                            api.emit('image', {
-                                buffer,
-                                filePath,
-                            })
-                        }
-                    }
-                )
+                api.emit("image", {
+                  buffer,
+                  filePath,
+                });
+              }
             }
-        },
-        debug,
-    })
+        );
+      }
+    },
+    debug,
+  });
 
-    return api
-}
+  return api;
+};
 
 export const generateAsync: (
     opts: DraftStabilityOptions & RequiredStabilityOptions
 ) => {
-    images: ImageData[]
-    res: ResponseData
+    images: ImageData[];
+    res: ResponseData;
 } = (opts) =>
-        // @ts-ignore
-        new Promise<{
-            images: ImageData[]
-            res: ResponseData
+// @ts-ignore
+  new Promise<{
+            images: ImageData[];
+            res: ResponseData;
         }>((resolve, reject) => {
-            const api = generate(opts)
-            let images: Array<ImageData> = []
-            api.on('image', (payload) => {
-                images = [...images, payload]
-            })
-            api.on('end', (data) => {
-                if (!data.isOk) console.error(data);//return reject(new Error(data.message))
-                resolve({
-                    res: data,
-                    images,
-                })
-            })
-        })
+          const api = generate(opts);
+          let images: Array<ImageData> = [];
+          api.on("image", (payload) => {
+            images = [...images, payload];
+          });
+          api.on("end", (data) => {
+            if (!data.isOk) console.error(data);
+            // return reject(new Error(data.message))
+            resolve({
+              res: data,
+              images,
+            });
+          });
+        });
