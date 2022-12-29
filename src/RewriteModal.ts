e510750895806ba873798bd40a8463771ba9097b
Benjamin Shafii <ben@prologe.io>
2022-12-29 22:03:51 +0100

    feat: add suggestions to rewrite modal

--- src/RewriteModal.ts
+++ src/RewriteModal.ts
@@ -1,7 +1,16 @@
-import { App, Modal, Setting } from 'obsidian';
+import { App, Modal, Setting, TextComponent } from 'obsidian';
+
+const suggestions = [
+  'Keep email addresses seperated by a comma',
+  'And explain it like I am 5',
+  'To translate to german',
+  'To use the metric system',
+  'Format this as a markdown table',
+];
 
 export class RewriteModal extends Modal {
   text: string;
+  textfield: TextComponent;
 
   onSubmit: (text: string) => void;
 
@@ -22,19 +31,36 @@ export class RewriteModal extends Modal {
     contentEl.createEl('h1', { text: 'Rewrite Assist' });
     const form = contentEl.createEl('form');
     form.onsubmit = this.search;
-    new Setting(form).setName('Rewrite text to').addText((text) => {
-      text
+
+    new Setting(form).setName('Rewrite text').addText((textfield) => {
+      textfield
         .setPlaceholder(
           'Remove all email addresses / Make it sound more like [paste text] / Make it more polite'
         )
+        .setValue(this.text)
         .onChange((value) => {
           this.text = value;
         });
-      text.inputEl.style.minWidth = '100%';
+      textfield.inputEl.style.minWidth = '100%';
+      this.textfield = textfield;
     });
 
-    // TextArea.inputEl.style.minHeight = '10rem';
-    // TextArea.inputEl.style.width = '100%';
+    // Display some suggestions
+    contentEl.createEl('h3', { text: 'Suggestions' });
+    const suggestionContainer = contentEl.createEl('div', {
+      attr: { style: 'display: flex; flex-wrap: wrap; gap: 0.5rem;' },
+    });
+    // add a few suggestions
+    suggestions.forEach((suggestion) => {
+      suggestionContainer.createEl('span', {
+        text: suggestion,
+        cls: 'setting-hotkey ',
+        attr: { style: 'width: min-content; padding: 0.5rem; cursor: pointer' },
+      }).onclick = () => {
+        this.text = suggestion;
+        this.textfield.setValue(suggestion);
+      };
+    });
 
     new Setting(form).addButton((btn) => {
       btn.buttonEl.type = 'submit';
