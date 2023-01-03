fc468571a6c9de2d43d9a1e3ba640d921b155642
Benjamin Shafii <ben@prologe.io>
2023-01-03 14:12:00 +0100

    chore: add a way to clear cache

--- src/LegacySettings.tsx
+++ src/LegacySettings.tsx
@@ -1,3 +1,4 @@
+import { Notice } from 'obsidian';
 import { posthog } from 'posthog-js';
 import * as React from 'react';
 import AvaPlugin from './main';
@@ -16,6 +17,7 @@ export const DEFAULT_SETTINGS: AvaSettings = {
 
 export function AdvancedSettings({ plugin }: { plugin: AvaPlugin }) {
   const [isDebug, setDebug] = React.useState(plugin.settings.debug);
+  const showAdvancedSettings = isDebug;
 
   const handleDebug = (event: React.ChangeEvent<HTMLInputElement>) => {
     const checked = event.target.checked;
@@ -24,27 +26,47 @@ export function AdvancedSettings({ plugin }: { plugin: AvaPlugin }) {
     setDebug(checked);
     checked && posthog.opt_out_capturing();
   };
+  // this will not refresh the ui but it will clear the cache
+  const handleDeleteCache = () => {
+    plugin.settings.token = undefined;
+    plugin.settings.vaultId = undefined;
+    plugin.saveSettings();
+    new Notice('Cache cleared');
+  };
 
   return (
-    <div className="relative flex items-start">
-      <div className="flex h-5 items-center">
-        <input
-          aria-describedby="comments-description"
-          name="comments"
-          type="checkbox"
-          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-          onChange={handleDebug}
-          checked={isDebug}
-        />
+    <div>
+      <div className="relative flex items-start">
+        <div className="flex h-5 items-center">
+          <input
+            aria-describedby="comments-description"
+            name="comments"
+            type="checkbox"
+            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+            onChange={handleDebug}
+            checked={isDebug}
+          />
+        </div>
+        <div className="ml-3 text-sm">
+          <label htmlFor="comments" className="font-medium ">
+            Debug
+          </label>
+          <p id="comments-description" className="text-gray-500">
+            You probably don't need this
+          </p>
+        </div>
       </div>
-      <div className="ml-3 text-sm">
-        <label htmlFor="comments" className="font-medium ">
-          Debug
-        </label>
-        <p id="comments-description" className="text-gray-500">
-          You probably don't need this
-        </p>
-      </div>
+      <button onClick={handleDeleteCache}>Delete Cache</button>
+      {showAdvancedSettings && (
+        <div className="overflow-x-auto mt-6">
+          <div>
+            <div>Token</div>
+            <pre className="select-text">{plugin.settings.token}</pre>
+            <div>Vault</div>
+            <pre className="select-text">{plugin.settings.vaultId}</pre>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
