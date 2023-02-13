6d49c83fc658fa72da8826bf6a8df8dab3cf2823
Benjamin Shafii <ben@prologe.io>
2023-02-13 18:09:38 +0100

    chore: remove codename

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -74,7 +74,7 @@ export const CustomSettings = ({ plugin }: { plugin: AvaPlugin }) => {
   const state = React.useSyncExternalStore(store.subscribe, store.getState);
   return (
     <div>
-      <div className="text-4xl mb-4">AVA - Codename AVA</div>
+      <div className="text-3xl mb-4">AVA</div>
       <div className="flex justify-between gap-3 mb-20">
         {!state.settings.token && (
           <div>You need to have an account to make the most of this plugin</div>
